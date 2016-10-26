615c425e545e2cee05c9ae25f881018a670d0444
Heark <adtune6509@student.waynecc.edu>
2016-10-26 14:16:23 -0400

    Update kinetics.js

--- kinetics.js
+++ kinetics.js
@@ -1,3 +1,14 @@
-function getDistance() {
+Object.prototype.squared = function() {
+ if( typeof this === int ) {
+  return this * this;
+ } else {
+ throw new Error("Only integers may be squared");
  }
  
+}
+var getDistance = function(time, acceleration, initial_velocity) {
+   this.time = t;
+   this.acceleration = a;
+   this.initial_veloticty = vi;
+ return vi * t + 0.5 * a * t.squared();
+}
